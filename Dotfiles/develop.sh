8456b9dd62be7349771546d975006937036a3d34
Bashar Eid <beesho.eid@gmail.com>
2025-02-23 22:57:14 -0500

    automate base docker file

---
+++ Dotfiles/develop.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+DOTFILES_DIRNAME=".files"
+DOTFILES_SRC_DIR="${HOME}/${DOTFILES_DIRNAME}"
+DOTFILES_COPY_DIRNAME="${DOTFILES_DIRNAME}.copy"
+DOTFILES_DEST_DIR="${SCRIPT_DIR}/${DOTFILES_COPY_DIRNAME}"
+
+if [[ -e "${DOTFILES_DEST_DIR}" ]]; then
+    rm -rf "${DOTFILES_DEST_DIR}"
+fi
+
+cp -r "${DOTFILES_SRC_DIR}" "${DOTFILES_DEST_DIR}"
+
+docker compose -f "${SCRIPT_DIR}/docker-compose.yaml" run --build --rm main
