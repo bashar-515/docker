9dec914dc3ad980260011f1c6040d16d29e3aeb4
Bashar Eid <beesho.eid@gmail.com>
2025-03-15 13:01:03 -0400

    don't use Docker cache

--- Dotfiles/build.sh
+++ Dotfiles/build.sh
@@ -5,4 +5,4 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 source "${SCRIPT_DIR}/.env"
-docker build --build-arg USERNAME=$USERNAME -t "${IMAGE_NAME}" $SCRIPT_DIR
+docker build --no-cache --build-arg USERNAME=$USERNAME -t "${IMAGE_NAME}" $SCRIPT_DIR
