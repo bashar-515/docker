e9e57f7b0e06e82bf1a5a265a2fd2b7344668d95
Bashar Eid <beesho.eid@gmail.com>
2025-03-16 23:49:46 -0400

    install more packages

--- Viam/build.sh
+++ Viam/build.sh
@@ -5,4 +5,4 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 source "${SCRIPT_DIR}/.env"
-docker build --build-arg USERNAME=$USERNAME --build-arg BASE_IMAGE=$BASE_IMAGE_NAME -t "${IMAGE_NAME}" $SCRIPT_DIR
+docker build --no-cache --build-arg USERNAME=$USERNAME --build-arg BASE_IMAGE=$BASE_IMAGE_NAME -t "${IMAGE_NAME}" $SCRIPT_DIR
