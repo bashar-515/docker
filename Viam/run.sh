da35a8561974d308c3d19cc8356e94a7e11e5232
Bashar Eid <beesho.eid@gmail.com>
2025-03-19 19:10:35 -0400

    finish installing dependencies for app

--- Viam/run.sh
+++ Viam/run.sh
@@ -5,4 +5,5 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 source "${SCRIPT_DIR}/.env"
-docker run --rm -it "${IMAGE_NAME}"
+
+docker run --rm -v ~/Developer/Git/Github/Code/Viam:/home/$USERNAME/Developer/Git/Github/Code/Viam -it "${IMAGE_NAME}"
