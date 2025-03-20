2d55cf0d9733c91a101b5ae41e8e0423dbd495fa
Bashar Eid <beesho.eid@gmail.com>
2025-03-20 00:20:42 -0400

    get almost everything working

--- Viam/run.sh
+++ Viam/run.sh
@@ -6,4 +6,15 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 source "${SCRIPT_DIR}/.env"
 
-docker run --rm -v ~/Developer/Git/Github/Code/Viam:/home/$USERNAME/Developer/Git/Github/Code/Viam -it "${IMAGE_NAME}"
+# TODO: get agent forwarding to work
+
+# eval "$(ssh-agent -s)"
+# ssh-add ~/.ssh/id_ed25519
+
+docker run -it --rm \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v ~/Developer/Git/Github/Code/Viam:/home/$USERNAME/Developer/Git/Github/Code/Viam \
+    -v ~/.ssh:/home/$USERNAME/.ssh "${IMAGE_NAME}"
+    # --mount type=bind,src=/run/host-services/ssh-auth.sock,target=/run/host-services/ssh-auth.sock \
+    # -e SSH_AUTH_SOCK="/run/host-services/ssh-auth.sock" \
+    # "${IMAGE_NAME}"
